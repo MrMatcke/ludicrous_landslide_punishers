61e66e5979889e29363a5572d4f3d4c681d68cd8
tonyrpark <tonyrpark@gmail.com>
2021-03-23 19:04:19 -0700

    adding some frontEnd files

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 // Import the custom helper methods
-const helpers = require('./utils/helpers');
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,12 +13,12 @@ const PORT = process.env.PORT || 3001;
 // Incorporate the custom helper methods
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
